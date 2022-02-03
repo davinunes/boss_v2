cf5e8ec0287f286347a6198ba996755d1a436dfe
davi.nunes@gmail.com <davi.nunes@gmail.com>
2022-02-03 19:30:02 -0300

    Atualização

--- fiberhome/listaOnline.php
+++ fiberhome/listaOnline.php
@@ -1,5 +1,5 @@
 <?php
-var_dump($_GET);
+// var_dump($_GET);
 
 $command = '/usr/bin/python /var/www/ilunne/boss/py/listaOnline.py '.$_GET[OLT].' '.$_GET[SLOT].' '.$_GET[PONN];
 
@@ -22,16 +22,37 @@ foreach($output as $a){
 	$item[mac] = $linha[5];
 	
 	$ixc = check_onu($item[mac]);
+	$status = check_precontrato($ixc[id_contrato]);
 	
 	$item[login] = $ixc[login];
 	$item[perfil] = $ixc[id_perfil];
 	$item[vlan] = $ixc[vlan];
+	$item[ativo] = $status;
 	
 	$onu[] = $item;
 	
 	
 }
-var_dump($onu);
+// var_dump($onu);
+echo "<table class='responsive-table'>\n";
+echo "	<thead>";
+echo "	</thead>";
+echo "  	<tbody>";
+
+foreach($onu as $t){
+	echo "<tr>
+	<td class=''>$t[num]</td>
+	<td>$t[ost]</td>
+	<td>$t[mac]</td>
+	<td>$t[vlan]</td>
+	<td>$t[perfil]</td>
+	<td>$t[ativo]</td>
+	<td>$t[login]</td>
+	</tr>";
+}
+
+echo "		</tbody>\n";
+echo "</table>\n";
 
 
 // view-source:https://acessodf.net/boss/fiberhome/listaOnline.php?OLT=172.21.2.2&SLOT=12&PONN=1
