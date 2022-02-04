6f436dfb536f2fc23ac5df04d4c18f03670b4437
davi.nunes@gmail.com <davi.nunes@gmail.com>
2022-02-04 17:00:02 -0300

    Atualização

--- fiberhome/listarOnline.js
+++ fiberhome/listarOnline.js
@@ -42,7 +42,34 @@ $(document).on('change', 'select#OLT', function(){
 	console.log(url);
 });
 $(document).on('change', 'select#SLOT', function(){
-	console.log($(this)[0].text.split("[")[1].split("]"));
+	let idx = $(this).val();
+	console.log(idx);
+	let txt = $("select#SLOT option[value="+idx+"]")[0].text.split("[")[1].split("]")[0];
+	
+	if(txt == "GC8B"){
+		$("#PONN").html("<option value='1'>1</option>\n<option value='2'>2</option>\n<option value='3'>3</option>\n<option value='4'>4</option>\n<option value='5'>5</option>\n<option value='6'>6</option>\n<option value='7'>7</option>\n<option value='8'>8</option>\n");
+	}else{
+		$("#PONN").html("
+			<option value='1'>1</option>\n
+			<option value='2'>2</option>\n
+			<option value='3'>3</option>\n
+			<option value='4'>4</option>\n
+			<option value='5'>5</option>\n
+			<option value='6'>6</option>\n
+			<option value='7'>7</option>\n
+			<option value='8'>8</option>\n
+			<option value='9'>9</option>\n
+			<option value='10'>10</option>\n
+			<option value='11'>11</option>\n
+			<option value='12'>12</option>\n
+			<option value='13'>13</option>\n
+			<option value='14'>14</option>\n
+			<option value='15'>15</option>\n
+			<option value='16'>16</option>\n
+		");
+	}
+	
+	$('select').formSelect();
 });
 
-// 
+// $("select#SLOT option")[0].text.split("[")[1].split("]")
