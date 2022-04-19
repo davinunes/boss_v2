b0e6d6c7b2f2d39a56fb0f7459102b3bf5bb8e5f
davi.nunes@gmail.com <davi.nunes@gmail.com>
2022-04-18 22:00:02 -0300

    Atualização

--- py/epon.py
+++ py/epon.py
@@ -13,14 +13,12 @@ password = 'admin'
 #defining the actual date to be add to the filename
 # data = arrow.now().format('DD-MM-YYYY_HH-mm')
 
-child = pexpect.spawn ('/usr/bin/ssh -o StrictHostKeyChecking=no '+user+'@'+HOST) #option needs to be a list
+child = pexpect.spawn ('/usr/bin/sshpass -p admin '+user+'@'+HOST) #option needs to be a list
 child.setwinsize(10000,10000)
 child.timeout = 150
-child.logfile = sys.stdout #display progress on screen
-child.expect('password:') #waiting for password
-child.sendline (password) #sending password
+child.logfile = sys.stdout
 time.sleep(2)
-child.expect('OLT8PON>')
+# child.expect('OLT8PON>')
 time.sleep(1)
 
 #go up enable configuration
