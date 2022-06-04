362d2fedd032ada61193322a66978f94b58cef36
davi.nunes@gmail.com <davi.nunes@gmail.com>
2022-06-04 18:00:08 -0300

    Atualização

---
+++ phpipam/app/tools/instructions/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *	print instructions
+ **********************************************/
+
+/* fetch instructions and print them in instructions div */
+$instructions = $Tools->fetch_object("instructions", "id", 1);
+$instructions = $instructions->instructions;
+
+/* format line breaks */
+$instructions = stripslashes($instructions);		//show html
+
+/* prevent <script> */
+$instructions = str_replace("<script", "<div class='error'><xmp><script", $instructions);
+$instructions = str_replace("</script>", "</script></xmp></div>", $instructions);
+
+// HSS header
+header('X-XSS-Protection:1; mode=block');
+
+if (!isset($i_am_a_widget))
+    print '<h4>'. _('Instructions for managing IP addresses'). '</h4><hr>';
+
+// Limit vertical height of instruction widget
+$style = isset($i_am_a_widget) ? 'style="display: block; text-overflow: ellipsis; word-wrap: break-word; overflow: hidden; max-height: 16em; line-height: 1em;"' : '';
+
+print '<div class="instructions well">';
+print "<div $style>". $instructions. '</div>';
+print '</div>';
